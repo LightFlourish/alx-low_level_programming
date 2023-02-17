b88d83366df3c5a0175277c66be8131b1d405789
LightFlourish <kemzykrimzy@gmail.com>
2023-02-16 18:03:57 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
 #include <unistd.h>
+#include <unixtd.h>
 /**
-  *
-  * Main - A c program that returns 1 
-  * Return 1
-  */
+ *
+ * Main - A c program that returns 1
+ * Return 1
+ */
 int main(void)
 {
-    const char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    const size_t len = sizeof(str) - 1;
-    ssize_t ret = write(STDERR_FILENO, str, len);
-    if (ret != len) {
-        return 1;
-    }
-    return 1;
-}
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}    
