f8963ac0cef50d0fe0b850341fecf6f51a2e4e96
LightFlourish <kemzykrimzy@gmail.com>
2023-02-19 15:24:41 -0800

    task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+# include <stdio.h>
 /**
- * main - main function
- *
- * Return: 0
- */
+  * main -main function
+  *
+  *Return: 0
+  */
 int main(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-	if (i != 'q && i != 'e')
-	putchar(i);
+		if (i != 'q' && i != 'e')
+			putchar(i);
 	putchar('\n');
 	return (0);
 }
